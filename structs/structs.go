3c2fe98fd6ba421dbd8958a06be862b456d937c9
lpsk <lipski@fastmail.com>
2022-08-17 21:37:59 +0200

    structs wip

--- structs/structs.go
+++ structs/structs.go
@@ -1,9 +1,14 @@
 package structs
 
-func Perimeter(w, h float64) float64 {
-	return 2 * (w + h)
+type Rectangle struct {
+	Width float64
+	Height float64
 }
 
-func Area(w, h float64) float64 {
-	return w * h
+func Perimeter(r Rectangle) float64 {
+	return 2 * (r.Width + r.Height)
+}
+
+func Area(r Rectangle) float64 {
+	return r.Height * r.Width
 }

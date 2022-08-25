dca17af1ba3c1ddcbbe1747e1af0214042618108
lpsk <lipski@fastmail.com>
2022-08-25 10:11:46 +0200

    finish structs chapter

--- structs/structs.go
+++ structs/structs.go
@@ -2,6 +2,11 @@ package structs
 
 import "math"
 
+type Shape interface {
+	Area() float64
+	Perimeter() float64
+}
+
 type Rectangle struct {
 	Width  float64
 	Height float64
@@ -21,4 +26,22 @@ type Circle struct {
 
 func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
+}
+
+func (c Circle) Perimeter() float64 {
+	return 2 * math.Pi * c.Radius
+}
+
+type RightTriangle struct {
+	Height float64
+	Base   float64
+}
+
+func (t RightTriangle) Area() float64 {
+	return (t.Height * t.Base) / 2
+}
+
+func (t RightTriangle) Perimeter() float64 {
+	ht := math.Sqrt(t.Height * t.Base)
+	return ht + t.Height + t.Base
 }

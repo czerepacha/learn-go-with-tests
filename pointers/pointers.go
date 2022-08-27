ba1912b71a5e59e4104175fb03e45b11d52674f3
lpsk <lipski@fastmail.com>
2022-08-27 22:13:09 +0200

    finished pointers and errors chapter

---
+++ pointers/pointers.go
@@ -0,0 +1,35 @@
+package pointers
+
+import (
+	"errors"
+	"fmt"
+)
+
+type Bitcoin int
+
+func (b Bitcoin) String() string {
+  return fmt.Sprintf("%d BTC", b)
+}
+
+type Wallet struct { 
+  balance Bitcoin
+}
+
+func (w *Wallet) Deposit(n Bitcoin) {
+  w.balance += n
+}
+
+func (w *Wallet) Balance() Bitcoin {
+  return w.balance
+}
+
+var ErrInsufficientFunds = errors.New("cannot withdraw, insufficient funds")
+
+func (w *Wallet) Withdraw(n Bitcoin) error {
+  if n > w.balance {
+    return ErrInsufficientFunds
+  }
+
+  w.balance -= n
+  return nil
+}
--- pointers/pointers_test.go
+++ pointers/pointers_test.go
@@ -2,3 +2,56 @@ package pointers
 
 import "testing"
 
+func TestWallet(t *testing.T) {
+
+  t.Run("deposit", func(t *testing.T) {
+    wallet := Wallet{balance: Bitcoin(0)}
+    wallet.Deposit(Bitcoin(10))
+
+    assertBalance(t, wallet, Bitcoin(10))
+  })
+
+  t.Run("withdrawal", func(t *testing.T) {
+    wallet := Wallet{balance: Bitcoin(20)}
+    err := wallet.Withdraw(Bitcoin(10))
+
+    assertNoError(t, err)
+    assertBalance(t, wallet, Bitcoin(10))
+  })
+
+  t.Run("withdraw insufficient funds", func(t *testing.T) {
+    wallet := Wallet{balance: Bitcoin(2)}
+    err := wallet.Withdraw(Bitcoin(3))
+
+    assertError(t, err, ErrInsufficientFunds)
+    assertBalance(t, wallet, Bitcoin(2))
+  })
+}
+
+
+func assertBalance(t *testing.T, w Wallet, want Bitcoin) {
+  t.Helper()
+  got := w.Balance()
+  
+  if got != want {
+    t.Errorf("got %s, want %s", got, want)
+  }
+}
+
+func assertNoError(t *testing.T, got error) {
+  t.Helper()
+  if got != nil {
+    t.Errorf("got %s instead of no error", got)
+  }
+}
+
+func assertError(t *testing.T, got, want error) {
+  t.Helper()
+  if got == nil {
+    t.Error("didn't get an error, but wanted one")
+  }
+
+  if got != want {
+    t.Errorf("got %q, want %q", got, want)
+  }
+}
